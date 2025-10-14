2fcaf6a7d4522c5a306e758664e692d7b837c5a9
Szymon Chmal <szymon@chmal.it>
2025-10-14 11:54:12 +0200

    feat: disable babel-preset when it's not needed

--- packages/babel-preset/src/preset.ts
+++ packages/babel-preset/src/preset.ts
@@ -1,7 +1,16 @@
 import globalRnHarnessPlugin from './global-plugin';
 import resolveWeakPlugin from './resolve-weak-plugin';
 
-export const rnHarnessPreset = () => {
+export const rnHarnessPreset = (api: import('@babel/core').ConfigAPI) => {
+  const isEnabled = !!process.env.RN_HARNESS;
+
+  api.cache.using(() => isEnabled);
+
+  if (!isEnabled) {
+    // Not a Harness build, so we don't need to do anything
+    return { plugins: [] };
+  }
+
   return {
     plugins: [
       '@babel/plugin-transform-class-static-block',
