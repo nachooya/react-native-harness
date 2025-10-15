3a61c3d41b8a90a5c66cb21b1f9e201ff6c90e65
Szymon Chmal <szymon@chmal.it>
2025-10-15 16:20:28 +0200

    fix: run babel preset all the time

--- packages/babel-preset/src/preset.ts
+++ packages/babel-preset/src/preset.ts
@@ -1,16 +1,8 @@
 import globalRnHarnessPlugin from './global-plugin';
 import resolveWeakPlugin from './resolve-weak-plugin';
 
-export const rnHarnessPreset = (api: import('@babel/core').ConfigAPI) => {
-  const isEnabled = !!process.env.RN_HARNESS;
-
-  api.cache.using(() => isEnabled);
-
-  if (!isEnabled) {
-    // Not a Harness build, so we don't need to do anything
-    return { plugins: [] };
-  }
-
+export const rnHarnessPreset = () => {
+  // Unfortunately, the Babel preset must be enabled at all times (at least for now).
   return {
     plugins: [
       '@babel/plugin-transform-class-static-block',
