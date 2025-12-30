90b0a49df388d36869e03436ccb24acec231fa9b
nacho <nachooya@gmail.com>
2025-12-30 13:54:23 +0100

    [Web]: Set WebDriver log level according to process.env.HARNESS_DEBUG

--- packages/platform-web/src/runner.ts
+++ packages/platform-web/src/runner.ts
@@ -1,23 +1,38 @@
-import { HarnessPlatformRunner } from '@react-native-harness/platforms';
 import WebDriver from 'webdriver';
+import { HarnessPlatformRunner } from '@react-native-harness/platforms';
 import { WebPlatformConfigSchema, type WebPlatformConfig } from './config.js';
+import logger from '@wdio/logger';
+
+const logLevel = !process.env.HARNESS_DEBUG ? 'silent' : 'info';
+
+logger.setLogLevelsConfig({
+  webdriver: logLevel,
+  '@wdio/utils': logLevel,
+  chromedriver: logLevel,
+  geckodriver: logLevel,
+});
 
 const getWebRunner = async (
   config: WebPlatformConfig
 ): Promise<HarnessPlatformRunner> => {
+
   const parsedConfig = WebPlatformConfigSchema.parse(config);
+
+  const capabilities: Record<string, string> = {
+    browserName: parsedConfig.browserName,
+  };
 
   const client = await WebDriver.newSession({
     path: '/',
-    capabilities: { browserName: parsedConfig.browserName },
-    logLevel: 'warn',
+    capabilities,
+    logLevel: logLevel,
   });
   return {
     startApp: async () => {
       await client.navigateTo(parsedConfig.appUrl);
     },
     restartApp: async () => {
-      await client.refresh();
+      await client.navigateTo(parsedConfig.appUrl);
     },
     stopApp: async () => {
       await client.navigateTo('about:blank');
