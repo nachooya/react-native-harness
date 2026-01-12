83b1ffb248316f4ec25a2d72c835465586f967e2
nacho <nachooya@gmail.com>
2026-01-12 13:35:38 +0100

    getCodeFrame: return null for web platform

--- packages/runtime/src/symbolicate.ts
+++ packages/runtime/src/symbolicate.ts
@@ -1,8 +1,12 @@
 import type { CodeFrame } from '@react-native-harness/bridge';
+import { Platform } from 'react-native';
 import parseErrorStack from 'react-native/Libraries/Core/Devtools/parseErrorStack';
 import symbolicateStackTrace from 'react-native/Libraries/Core/Devtools/symbolicateStackTrace';
 
 export const getCodeFrame = async (error: Error): Promise<CodeFrame | null> => {
+  if (Platform.OS === 'web') {
+    return null;
+  }
   const parsedStack = parseErrorStack(error.stack);
   const symbolicatedStack = await symbolicateStackTrace(parsedStack);
 
